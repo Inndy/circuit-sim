81674b01fe20489fb0d8f616287beddeee02116f
Inndy <inndy.tw@gmail.com>
2022-02-09 22:59:40 +0800

    Implement test for TestRotateRight, TestRotateLeft, ShiftRight

--- bitvector_test.go
+++ bitvector_test.go
@@ -22,9 +22,61 @@ func TestNot(t *testing.T) {
 	}
 }
 
-// func TestRotateRight
-// func TestRotateLeft
-// func (v *BitVector) ShiftRight(n int) *BitVector {
+func rotateRightU64(v uint64, s, w int) uint64 {
+	mask := uint64(0xffffffffffffffff) >> (64 - w)
+	return ((v >> s) | (v << (w - s))) & mask
+}
+
+func TestRotateRight(t *testing.T) {
+	rand.Seed(0)
+
+	input := NewInputVector(64)
+	for i := 0; i < TestLoopCount; i++ {
+		a := rand.Uint64()
+		s := i % 64
+		v := rotateRightU64(a, s, 64)
+		compute := input.RotateRight(s)
+		input.SetUint64(a)
+
+		if r := compute.Eval().GetUint64(); r != v {
+			t.Errorf("TestRotateRight %x >> %d = %x (expect %x)", a, s, r, v)
+		}
+	}
+}
+
+func TestRotateLeft(t *testing.T) {
+	rand.Seed(0)
+
+	input := NewInputVector(60)
+	for i := 0; i < TestLoopCount; i++ {
+		a := rand.Uint64() >> 4
+		s := i % 60
+		v := rotateRightU64(a, 60 - s, 60)
+		compute := input.RotateLeft(s)
+		input.SetUint64(a)
+
+		if r := compute.Eval().GetUint64(); r != v {
+			t.Errorf("TestRotateLeft %x >> %d = %x (expect %x)", a, s, r, v)
+		}
+	}
+}
+
+func TestShiftRight(t *testing.T) {
+	rand.Seed(0)
+
+	input := NewInputVector(64)
+	for i := 0; i < TestLoopCount; i++ {
+		a := rand.Uint64()
+		s := i % 64
+		v := a >> s
+		compute := input.ShiftRight(s)
+		input.SetUint64(a)
+
+		if r := compute.Eval().GetUint64(); r != v {
+			t.Errorf("TestShiftRight %x >> %d = %x (expect %x)", a, s, r, v)
+		}
+	}
+}
 
 func TestAnd(t *testing.T) {
 	rand.Seed(0)
